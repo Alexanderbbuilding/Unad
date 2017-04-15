ea9ee62fdd78cb00c4ef01bc315b446c5fec222b
Alexanderbbuilding <Alexander.gonzalez1995@gmail.com>
2017-04-15 09:49:03 -0500

    actualizacion rep
    base2

--- Administracion/templates/sidebar.php
+++
@@ -1,5 +0,0 @@
-<!-- Sidebar -->
-<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">   	
-	
-</div> 
-
--- Conexion/Conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-$host="localhost";
-$user="root";
-$pass="";
-$dbname="cdi_valoracion";
-$conexion = mysqli_connect($host,$user,$pass,$dbname);
-?>
